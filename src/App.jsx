95f0acbaa5d6bdacc218cb94b65e8e287b86bf1d
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-06-21 13:35:18 -0300

    feature: created post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
@@ -13,7 +14,16 @@ function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post 
+            author="Luis Paulo Ruda" 
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur minima eum molestias. Architecto quia quibusdam cum laboriosam accusamus ipsum, illo consequuntur sunt, esse vero reiciendis dolores nihil ab veritatis magni!"
+          />
+          <Post 
+            author="Luis P." 
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur minima eum molestias. Architecto quia quibusdam cum laboriosam accusamus ipsum, illo consequuntur sunt, esse vero reiciendis dolores nihil ab veritatis magni!"
+          />
+        </main>
       </div>
     </div>
   )
---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/LuisRuda.png" />
+          <div className={styles.authorInfo}>
+            <strong>Luis Paulo Ruda</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="21 de junho ás 13:28h" dateTime="2022-06-21 13:28:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
