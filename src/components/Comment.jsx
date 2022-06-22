380f0c3266366754f32473374d6ca39d8e0e566e
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-06-22 11:13:12 -0300

    feature: created comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+import styles from './Comment.module.css';
+
+export function Comment () {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/LuisRuda.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luis Paulo Ruda</strong>
+              <time 
+                title="21 de junho ás 13:28h" 
+                dateTime="2022-06-21 13:28:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
