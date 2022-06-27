29931944640ef7bcb3b35fb64efe6b50320b5a2c
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-06-27 08:32:23 -0300

    feature: using likes in comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 import { Avatar } from '../components/Avatar';
@@ -5,8 +6,14 @@ import { Avatar } from '../components/Avatar';
 import styles from './Comment.module.css';
 
 export function Comment ({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(oldLikes => oldLikes + 1);
   }
 
   return (
@@ -35,9 +42,9 @@ export function Comment ({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
